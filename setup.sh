4f84ad6de2f75c6b46416fd632c36f3cbbf19884
Alberto Zaccagni <montecristoh@gmail.com>
2015-12-01 00:26:06 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt-get install -y python python-dev python-pip git git-core openssh-server
-sudo pip install ansible
+sudo pip install ansible markupsafe
 
 ansible-playbook -i hosts inventory.yml -c local -K
