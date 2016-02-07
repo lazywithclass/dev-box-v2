1c90076175ded30d927518844b22c935f30d0972
Alberto Zaccagni <montecristoh@gmail.com>
2016-02-07 21:16:42 +0000

    install ansible in both linux and macosx then proceed

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sudo apt-get install -y python python-dev python-pip git git-core openssh-server
-sudo pip install ansible markupsafe
+if which apt-get > /dev/null; then
+  sudo apt-get install -y python python-dev python-pip git git-core openssh-server
+  sudo pip install ansible markupsafe
+else
+  brew install ansible
+fi
 
 ansible-playbook -i inventory playbook.yml -c local -K
