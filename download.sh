107df1ac2464340666a10e40efa53de9ed755d3d
Alberto Zaccagni <montecristoh@gmail.com>
2016-02-07 21:34:17 +0000

    reverted

--- download.sh
+++ download.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if which apt-get > /dev/null; then
-  sudo apt-get install -y git
-else
-  brew install git
-fi
-
+sudo apt-get install -y git
 mkdir ~/workspace
 git clone git@github.com:lazywithclass/dev-box-v2.git ~/workspace/dev-box-v2
