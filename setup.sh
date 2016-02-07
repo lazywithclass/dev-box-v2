5746163922c2a2a1ed1cd6d1d1f4c141dfb4e550
Alberto Zaccagni <montecristoh@gmail.com>
2016-02-07 21:34:42 +0000

    reverted

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if which apt-get > /dev/null; then
-  sudo apt-get install -y python python-dev python-pip git git-core openssh-server
-  sudo pip install ansible markupsafe
-else
-  brew install ansible
-fi
-
+sudo apt-get install -y python python-dev python-pip git git-core openssh-server
+sudo pip install ansible markupsafe
 ansible-playbook -i inventory playbook.yml -c local -K
