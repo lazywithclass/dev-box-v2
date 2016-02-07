47c24255c9bd4a5cb23f046ead5070002f52fd87
Alberto Zaccagni <montecristoh@gmail.com>
2016-02-07 20:27:26 +0000

    install git in both linux and macosx then proceed

--- download.sh
+++ download.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-sudo apt-get install -y git
+if which apt-get > /dev/null; then
+  sudo apt-get install -y git
+else
+  brew install git
+fi
+
 mkdir ~/workspace
 git clone git@github.com:lazywithclass/dev-box-v2.git ~/workspace/dev-box-v2
